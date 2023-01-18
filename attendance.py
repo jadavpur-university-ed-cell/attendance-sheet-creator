6af4db87a51783eb04ed72df8d7cc49a912e7922
prannay <prannaykedia1@gmail.com>
2023-01-19 01:33:00 +0530

    updated the if else condition in the code

--- attendance.py
+++ attendance.py
@@ -13,12 +13,12 @@ all_years = ['UG-1', 'UG-2', 'UG-3', 'UG-4']
 for title in session_titles:
     num_students = random.randint(32, 54)
     random_students_list = random.sample(names_list, num_students)
-    if title == 'CII':
-        dept_list = [random.choice(all_depts) for i in range(num_students)]
-        yr_list = [random.choice(all_years) for i in range(num_students)]
-    else:
+    if title == 'Chem Engineering' or title == 'Power Engineering':
         dept_list = [title for i in range(num_students)]
         yr_list = ['UG-1' for i in range(num_students)]
+    else:
+        dept_list = [random.choice(all_depts) for i in range(num_students)]
+        yr_list = [random.choice(all_years) for i in range(num_students)]
     sr_num_list = [i for i in range(1, num_students+1)]
     data = {
         'Sr No.': sr_num_list,
